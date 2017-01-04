d4621c4f58dd6fabd3758841617ed7357ec70e66
marcelduerr <marcel.duerr@yahoo.de>
2017-01-04 13:59:47 +0100

    update node_to_cell and rhs_integration

--- fem-lab/functions/right hand side/rhs_integration.m
+++ fem-lab/functions/right hand side/rhs_integration.m
@@ -1,20 +1,23 @@
-function rhs = rhs_integration(polynomial_deg,mesh_size,cell,vertix)
+function rhs = rhs_integration(mesh_size,polynomial_deg,cell,vertex,SF)
 
     cells_per_row=(1/mesh_size);
     number_of_nodes=((polynomial_deg*cells_per_row)+1)^2;
     
     for i=1:number_of_nodes
-        RHS=0
-        A=node_nodes_to_cells(i,mesh_size,polynomial_deg)
-        active_cells=A(:,1)
-        active_sf=A(:,2)    
-        for k=1:rows(active_cells)
-            active_cell_no=active_cells(k)
-            active_sf_no=active_sf(k)
-            f = @(x,y) cos(mesh_size*x*pi+vertix(active_cell,1)*cos(mesh_size*y*pi+vertix(active_cell,2).*hf_eval_poly(x,y,SF(active_sf_no))
-            RHS=RHS+dblquad(f,0,1,0,1)
+        RHS=0;
+        i
+        [active_cell,active_sf]=node_node_to_cells(i,mesh_size,polynomial_deg)
+        for k=1:rows(active_cell)
+            active_cell_no=active_cell(k);
+            active_vertex=vertex(active_cell_no,:);
+            active_sf_no=active_sf(k);
+            f = @(x,y) cos(mesh_size*x*pi+active_vertex(1))*cos(mesh_size*y*pi+active_vertex(2)).*hf_eval_poly(x,y,SF(active_sf_no,:));
+            integral=dblquad(f,0,1,0,1);
+            RHS=RHS+integral;
         endfor
-        rhs(i)=RHS
+        rhst(i)=RHS;
     endfor
 
+    rhs=mesh_size^2*rhst';
+    
 endfunction
