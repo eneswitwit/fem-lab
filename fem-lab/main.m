6d30131f4e0b0518b35b0e02a7534105db0b5c49
Enes Witwit <Witwit@dhcp662-058.laptop-wlc.uni-heidelberg.de>
2017-01-22 22:45:40 +0100

    Structured main.m

--- fem-lab/main.m
+++ fem-lab/main.m
@@ -1,7 +1,7 @@
 function main()
-    tic
     % Add all subfolders to working directory
     addpath(genpath([pwd '\functions']))
+    
     % Initialize parameters mesh-size and polynomial degree
     mesh_size=1/64;
     pol_deg=1;
@@ -10,27 +10,45 @@ function main()
     % Initialize exact solution
     u_exact=@(x,y) cos(x*pi).*cos(y*pi)*(1/(1+2*pi*pi));
     
+    % Start dial gauge
+    tic
+    
     % Initialize our mesh and our coefficient matrix for the shape functions
     [Vertex,Cell]=mesh_generate(mesh_size);
     SF=sf_generate(pol_deg);
+    
+    % ------------------------ FEM -------------------------------
     
     % sm_assemble_global will give the global stiffness matrix
     % sm_assemble_global will call the function sm_assemble_local, which initializes the local stiffness matrix
     SM=sm_assemble_global(mesh_size,pol_deg);
     disp("------------------Assembled global matrix------------------");
+    
     % Initialize right hand side of our linear system
     rhs=rhs_integration(mesh_size,SF,f);
     disp("------------------Assembled right hand side------------------");
+    
     % solve the linear system using ls_solve
     u_coeff=ls_solve(SM,rhs);
     disp("------------------Solved linear system------------------");
+    
     % hf_eval_solution is used to evaluate the approximation, given the coefficients u_coeff;
     u=@(x,y) hf_eval_solution(x,y,u_coeff,Cell,Vertex,pol_deg,SF);
+    
+    % ------------------------------------------------------------------
+    
+    % --- Plotting ---
     % m_plot will plot the approximation, the exact solution and the right hand side of the strong form to illustrate, that it is in fact a multiple of the solution
     m_plot_solution(u_coeff,pol_deg,mesh_size);
+    % ----------------
+    
+    % --- Error Analysis ---
+    % error_runge will compute an estimation of the L2 Error
+    error_runge(mesh_size,pol_deg);
     % error_L2 computes the error, using the exact solution and the L2-norm
     error_L2(u,u_exact,10);
-    error_runge(0.1,1)
+    % ----------------------
+   
     % overall runtime using 'tic' 'toc'
     overall_runtime=toc
     
