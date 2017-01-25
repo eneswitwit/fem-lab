10fca4d6e94c095507152cae4d9d56167c82448c
Enes Witwit <Witwit@Eness-MacBook-Air.local>
2017-01-25 22:30:02 +0100

    Plotting with scatter

--- fem-lab/main.m
+++ fem-lab/main.m
@@ -5,7 +5,7 @@ function main()
     for n=3:3;
         
         % Initialize parameters mesh-size and polynomial degree
-        mesh_size=1/(2^n);
+        mesh_size=1/2;
         pol_deg=1;
         % Initialize right hand side of strong formulation
         f = @(x,y) cos(x*pi).*cos(y*pi);
