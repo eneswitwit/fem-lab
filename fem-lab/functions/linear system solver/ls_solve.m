53b4c07551b8b67e7acbf8fb8ceea284ee15de2b
Enes Witwit <Witwit@dhcp560-191.laptop-wlc.uni-heidelberg.de>
2017-02-04 00:35:42 +0100

    Structure.

--- fem-lab/functions/linear system solver/ls_solve.m
+++ fem-lab/functions/linear system solver/ls_solve.m
@@ -1,30 +1,10 @@
-function [x,ls_error_cg,ls_error_fgm,ls_error_min, minres_runtime, cg_runtime,gmres_runtime] = ls_solve(A,b)
-
-    % FGMRES
-    tic
-    x1= ls_fgmres(A,b,10,10^(-10),50,eye(rows(A)),b);
-    gmres_runtime=toc;
-    
-    % MINRES
-    tic
-    x2= ls_minres(A,b,b,100,10^(-10))
-    minres_runtime=toc;
-    
-    % CG Linear solver
-    tic
-    x3=ls_cg(A,b,b);
-    cg_runtime=toc;
-    
-    % Error by linear solver
-    ls_error_fgm=(A*x1-b)'*(A*x1-b);
-    ls_error_cg=(A*x3-b)'*(A*x3-b);
-    ls_error_min=(A*x2-b)'*(A*x2-b);
-    
-    if (ls_error_cg<ls_error_fgm)
-        x=x2;
-    else
-        x=x1;
-    endif
-
-    
+function [solution, residual] = ls_solve(A,b)
+    % Test whether length of matrix is bigger than 10201. This value is computed by linear_solver_analytics.m.
+    % That value tells us that GMRES is better for linear systems of the size 10201 ^2 and bigger. Otherwise MINRES is the better choice.
+    if(length(A) > 10201)
+        solution = gmres(A,b);
+        residual = abs(b-A*solution);
+     else
+        [solution,residual] = ls_minres(A,b,b,100,10^(-10));
+     endif
 end
--- fem-lab/main.m
+++ fem-lab/main.m
@@ -1,16 +1,16 @@
 function [error_L2 , overall_runtime] = main(mesh_size = 1/4 , pol_deg = 2)
-    % Add all subfolders to working directory
+    % Add all subfolders to working directory.
     add_fem_lab();
             
-    % Initialize right hand side of strong formulation
+    % Initialize right hand side of strong formulation.
     f = @(x,y) cos(x*pi).*cos(y*pi);
-    % Initialize exact solution
+    % Initialize exact solution.
     u_exact=@(x,y) cos(x*pi).*cos(y*pi)*(1/(1+2*pi*pi));
     
-    % Start dial gauge
+    % Start dial gauge.
     t0 = clock();
     
-    % Initialize our mesh and our coefficient matrix for the shape functions
+    % Initialize our mesh and our coefficient matrix for the shape functions.
     [Vertex,Cell]=mesh_generate(mesh_size);
     SF=sf_generate(pol_deg);
     
@@ -22,41 +22,32 @@ function [error_L2 , overall_runtime] = main(mesh_size = 1/4 , pol_deg = 2)
     disp("------------------Assembled global matrix------------------");
     fflush(stdout);
     
-    % Initialize right hand side of our linear system
-    %tic
+    % Initialize right hand side of our linear system.
     rhs=rhs_integration_vectorized(Vertex,Cell,SF,f);
-    %rhstime=toc
     disp("------------------Assembled right hand side------------------");
     fflush(stdout);
     
-    % solve the linear system using ls_solve
-    %[u_coeff,ls_error_cg,ls_error_fgm,ls_error_min, minres_runtime, cg_runtime,gmres_runtime] = ls_solve(SM,rhs);
-    u_coeff = ls_minres(SM,rhs,rhs,100,10^(-10));
+    % Solve the linear system,
+    u_coeff = ls_solve(SM,rhs);
     disp("------------------Solved linear system------------------");
     fflush(stdout);
     
-    % hf_eval_solution is used to evaluate the approximation, given the coefficients u_coeff;
+    % hf_eval_solution is used to evaluate the approximation, given the coefficients u_coeff.
     u=@(x,y) hf_eval_solution(x,y,u_coeff,Cell,Vertex,SF);
     
     % ------------------------------------------------------------------
         
-    % Measure runtime needed for solving FEM problem.
+    % Measure runtime needed for solving FEM.
     overall_runtime = etime(clock() , t0);
 
     % ------- Plotting --------
     % m_plot will plot the approximation, the exact solution and the right hand side of the strong form to illustrate, that it is in fact a multiple of the solution
     m_plot_solution(u_coeff,Cell,Vertex,SF);
     % -------------------------
-        
-        
+           
     % ------ Error Analysis ------
-    
-    % error_runge will compute an estimation of the L2 Error
-    %error_runge(0.2,pol_deg);
-    
     % error_L2 computes the error, using the exact solution and the L2-norm
     error_L2 = error_L2(u,u_exact,10);
-    
     % ----------------------------
         
     
