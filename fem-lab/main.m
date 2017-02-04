78cb46abeb83a3fb7c3be49e886acb749a8777a8
Marcel <marcel.duerr@yahoo.de>
2017-02-04 03:26:26 +0100

    error map main update

--- fem-lab/main.m
+++ fem-lab/main.m
@@ -13,23 +13,26 @@ function [error_L2 , overall_runtime, u_coeff] = main(mesh_size = 1/4 , pol_deg 
     % Initialize our mesh and our coefficient matrix for the shape functions.
     [Vertex,Cell]=mesh_generate(mesh_size);
     SF=sf_generate(pol_deg);
-    
+    disp("---------------   Start FEM");
+    disp(["---------------   Polyonmial Degree " num2str(pol_deg)]);
+    disp(["---------------   Mesh Size 1/" num2str(1/mesh_size)]);  
+    fflush(stdout);  
     % ------------------------ FEM -------------------------------
     % sm_assemble_local computes the local stiffness matrix. In our case,the local stiffness matrix looks the same for every cell.
     SM_local=sm_assemble_local_vectorized(mesh_size,SF);
     % sm_assemble_global will give the global stiffness matrix
     SM=sm_assemble_global(mesh_size,pol_deg,SM_local);
-    disp("------------------Assembled global matrix--------------------");
+    disp("---------------   Assembled global matrix");
     fflush(stdout);
     
     % Initialize right hand side of our linear system.
     rhs=rhs_integration_vectorized(Vertex,Cell,SF,f);
-    disp("------------------Assembled right hand side------------------");
+    disp("---------------   Assembled right hand side");
     fflush(stdout);
     
     % Solve the linear system,
     u_coeff = ls_solve(SM,rhs);
-    disp("------------------Solved linear system-----------------------");
+    disp("---------------   Solved linear system");
     fflush(stdout);
     
     % hf_eval_solution is used to evaluate the approximation, given the coefficients u_coeff.
@@ -48,7 +51,8 @@ function [error_L2 , overall_runtime, u_coeff] = main(mesh_size = 1/4 , pol_deg 
     % ------ Error Analysis ------
     % error_L2 computes the error, using the exact solution and the L2-norm
     error_L2 = error_L2(u,u_exact,10);
-    disp(['------------------L2 error: ' num2str(error_L2) '-----------------------']);
+    disp(['---------------   L2 error: ' num2str(error_L2)]);
+    disp(' ');
     fflush(stdout);
     % ----------------------------
           
