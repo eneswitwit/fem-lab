fdf392cc72977a63067cfdbd29f5164a56449cb1
Marcel <marcel.duerr@yahoo.de>
2017-02-04 03:43:44 +0100

    comments

--- fem-lab/functions/help function/hf_remove_duplicates.m
+++ fem-lab/functions/help function/hf_remove_duplicates.m
@@ -1,7 +1,8 @@
 function [active_node,local_node,active_cell_vector]=hf_remove_duplicates(active_node,local_node,active_cell_vector)
-
+    % We wil check for duplicate global nodes, which get accessed by more than one cell
     for i=1:length(active_node)-1
         for j=i+1:length(active_node)
+            % if two active nodes are the same, one will get ommited
             if (active_node(j)==active_node(i))
                 active_node(j)=0;
                 local_node(j)=0;
@@ -10,6 +11,9 @@ function [active_node,local_node,active_cell_vector]=hf_remove_duplicates(active
         end
     endfor
     
+    % Every duplicate was swapped with a zero columns
+    % Octaves function 'unique' will ommit non-unique columns
+    % If the first column is a zero column, it will be skipped
     Aux_matrix=unique([active_node',local_node',active_cell_vector'],"rows");
     if(Aux_matrix(1,1)==0)
         active_node=Aux_matrix(2:rows(Aux_matrix),1)';
